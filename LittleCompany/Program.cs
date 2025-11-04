dffbe2dc89c89690086d87118c1b0c2dd62b29ef
Elev <Elev@DESKTOP-BCIRL0H>
2025-11-04 14:15:30 +0100

    Add project files.

---
+++ LittleCompany/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LittleCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
