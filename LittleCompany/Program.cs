4675966b8b8bc66f1740f147e70dd0f1a7f68405
faal <130977909+faal2@users.noreply.github.com>
2025-11-04 14:24:09 +0100

    Som en del av steg 6

--- LittleCompany/Program.cs
+++ LittleCompany/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Första commit
+            Console.WriteLine("Första commit");
         }
     }
 }
