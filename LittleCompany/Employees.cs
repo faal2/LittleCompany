627f978c5a1a6f6540cdf09385d95ac35f5fc0e7
faal <130977909+faal2@users.noreply.github.com>
2025-11-05 10:38:53 +0100

    seperate method

---
+++ LittleCompany/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleCompany
+{
+    internal class Employees
+    {
+        //static Personal addEmployee(string name, int salary)
+        //{
+        //    Personal personal = new Personal(name, salary);
+        //    return personal;
+        //}
+    }
+}
